e40eefeb9c87765912d6eb6737464458f8b2d034
Elias <eludebebe@gmail.com>
2021-11-05 15:36:26 +0300

    biometric registration

---
+++ registrationclient/app/src/main/java/io/mosip/registration/app/BiometricRegistrationController.java
@@ -0,0 +1,14 @@
+package io.mosip.registration.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BiometricRegistrationController extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.biometric_registration_controller);
+    }
+}
