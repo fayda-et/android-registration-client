d7371d0b001e7126cb60af98fbce06bcccaa7f6c
Elias <eludebebe@gmail.com>
2021-11-19 09:49:17 +0300

    biometric with iris exception

---
+++ registrationclient/app/src/main/java/io/mosip/registration/app/Constants.java
@@ -0,0 +1,8 @@
+package io.mosip.registration.app;
+
+public class Constants
+{
+    public static String LEFT_IRIS_SUB_TYPE="LEFT_IRIS";
+    public static String RIGHT_IRIS_SUB_TYPE="RIGHT_IRIS";
+    public static String IRIS="IRIS";
+}
